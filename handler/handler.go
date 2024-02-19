ec8bca3332c6a22f2ab4a6376a7936337ffc06c9
Jean Freitas <jeannsfreitas@gmail.com>
2024-02-18 21:36:45 -0300

    feat: add createOpening handler logic

--- handler/handler.go
+++ handler/handler.go
@@ -1,7 +1,20 @@
 package handler
 
+import (
+	"github.com/jeannsf/Gopportunities/config"
+	"gorm.io/gorm"
+)
 
 
+var (
+	logger *config.Logger
+	db *gorm.DB
+)
+
+func InitializeHandler(){
+	logger = config.GetLogger("Handler")
+	db = config.GetSQLite()
+}
 
 
 
